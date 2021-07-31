5fe1ef5fa78073ca362fb3f6a57b9deb59b585f4
Daniel Garnica Sánchez <thedaniel115@gmail.com>
2021-07-31 01:00:15 -0500

    ADD i18n & Apollo Config

---
+++ lib/i18n/enUS.ts
@@ -0,0 +1,3 @@
+export const enUS = {
+    helloWorld: 'Hello World',
+};
--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    i18n: {
+        locales: ['es-MX', 'en-US'],
+        defaultLocale: 'es-MX',
+        localeDetection: false, // TODO: Remove this in production
+    },
+    images: {
+        domains: [
+            'storage.googleapis.com',
+            'images.unsplash.com',
+            'storage.cloud.google.com',
+        ],
+    },
+};
