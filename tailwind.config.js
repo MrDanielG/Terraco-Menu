23af7854c9206aebd5b71ce321d24533889be720
Daniel Garnica Sánchez <thedaniel115@gmail.com>
2021-08-01 21:42:21 -0500

    UPDATE Basic Menu Layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,14 @@ module.exports = {
                     DEFAULT: '#401D0A',
                 },
                 orange: {
-                    light: '#FD8D00',
+                    lighter: '#FD8D00',
+                    light: '#FF6E3A',
                     DEFAULT: '#FA4A0C',
+                    dark: '#E31B00',
                 },
+                backgroundImage: (theme) => ({
+                    login: "url('https://images.unsplash.com/photo-1627844642677-8b30cb8fc636?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')",
+                }),
             },
         },
     },
