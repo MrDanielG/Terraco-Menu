b044365794a5b0abf23d87c195016ea297cd77ff
Daniel Garnica Sánchez <thedaniel115@gmail.com>
2021-08-17 02:02:38 -0500

    Update Loader to Skeleton

--- components/ProtectedPage.tsx
+++ components/ProtectedPage.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import LoadingOverlay from 'react-loading-overlay';
+import ContentLoader from 'react-content-loader';
 import { useAuth } from '../contexts/AuthContext';
 interface ProtectedProps {
     children: JSX.Element | JSX.Element[];
@@ -28,9 +28,21 @@ export function ProtectedPage({ children, username, redirectTo }: ProtectedProps
     return (
         <>
             {loading ? (
-                <LoadingOverlay active={loading} spinner text="Loading...">
-                    <div className="min-h-screen"></div>
-                </LoadingOverlay>
+                <div className="p-12 min-h-screen w-full flex justify-center items-center">
+                    <ContentLoader
+                        speed={2}
+                        viewBox="0 0 200 800"
+                        backgroundColor="#f1f1f1"
+                        foregroundColor="#ebebeb"
+                    >
+                        <rect x="48" y="8" rx="3" ry="3" width="88" height="6" />
+                        <rect x="48" y="26" rx="3" ry="3" width="52" height="6" />
+                        <rect x="0" y="56" rx="3" ry="3" width="410" height="6" />
+                        <rect x="0" y="72" rx="3" ry="3" width="380" height="6" />
+                        <rect x="0" y="88" rx="3" ry="3" width="178" height="6" />
+                        <circle cx="20" cy="20" r="20" />
+                    </ContentLoader>
+                </div>
             ) : (
                 children
             )}
