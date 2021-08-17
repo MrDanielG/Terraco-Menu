ab2f139d97d48324939af6a3487974c93f951850
Daniel Garnica Sánchez <thedaniel115@gmail.com>
2021-08-17 01:30:16 -0500

    Add Protected Route to Manager

--- pages/manager/index.tsx
+++ pages/manager/index.tsx
@@ -2,21 +2,24 @@ import React from 'react';
 import ParentCard from '../../components/cards/parent-card/ParentCard';
 import StatsCard from '../../components/cards/StatsCard';
 import Navbar from '../../components/layout/Navbar';
+import ProtectedPage from '../../components/ProtectedPage';
 
 interface Props {}
 
 const index = (props: Props) => {
     return (
-        <div className="bg-gray-200 p-8 h-screen">
-            <Navbar />
-            <h1 className="font-semibold text-3xl text-brown">Estadísticas</h1>
+        <ProtectedPage username="Manager" redirectTo="/">
+            <div className="bg-gray-200 p-8 h-screen">
+                <Navbar />
+                <h1 className="font-semibold text-3xl text-brown">Estadísticas</h1>
 
-            <h2 className="mt-10 mb-6 text-brown text-lg">Platillo del Mes:</h2>
-            <ParentCard />
+                <h2 className="mt-10 mb-6 text-brown text-lg">Platillo del Mes:</h2>
+                <ParentCard />
 
-            <h2 className="mt-10 mb-6 text-brown text-lg">Ventas Mensuales</h2>
-            <StatsCard />
-        </div>
+                <h2 className="mt-10 mb-6 text-brown text-lg">Ventas Mensuales</h2>
+                <StatsCard />
+            </div>
+        </ProtectedPage>
     );
 };
 
--- pages/manager/stats/dishes.tsx
+++ pages/manager/stats/dishes.tsx
@@ -1,4 +1,6 @@
 import Navbar from '../../../components/layout/Navbar';
+import ProtectedPage from '../../../components/ProtectedPage';
+
 interface Props {}
 
 const months = [
@@ -26,55 +28,57 @@ const months = [
 
 const SellsStats = (props: Props) => {
     return (
-        <div className="bg-gray-200 p-8 min-h-screen">
-            <Navbar />
-            <h1 className="font-semibold text-3xl text-brown">Platillos del Mes</h1>
+        <ProtectedPage username="Manager" redirectTo="/">
+            <div className="bg-gray-200 p-8 min-h-screen">
+                <Navbar />
+                <h1 className="font-semibold text-3xl text-brown">Platillos del Mes</h1>
 
-            <div className="flex flex-col mt-6">
-                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                            <table className="min-w-full divide-y divide-gray-200">
-                                <thead className="bg-gray-50">
-                                    <tr>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                            Mes
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                            Ingreso
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="bg-white divide-y divide-gray-200">
-                                    {months.map((month, i) => (
-                                        <tr key={i}>
-                                            <td className="px-6 py-4 whitespace-nowrap">
-                                                <div className="flex items-center">
-                                                    <div className="text-sm font-medium text-gray-800">
-                                                        {month.name}
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className="px-6 py-4">
-                                                <div className="text-sm text-gray-500">
-                                                    {month.income}
-                                                </div>
-                                            </td>
+                <div className="flex flex-col mt-6">
+                    <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <table className="min-w-full divide-y divide-gray-200">
+                                    <thead className="bg-gray-50">
+                                        <tr>
+                                            <th
+                                                scope="col"
+                                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >
+                                                Mes
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >
+                                                Ingreso
+                                            </th>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody className="bg-white divide-y divide-gray-200">
+                                        {months.map((month, i) => (
+                                            <tr key={i}>
+                                                <td className="px-6 py-4 whitespace-nowrap">
+                                                    <div className="flex items-center">
+                                                        <div className="text-sm font-medium text-gray-800">
+                                                            {month.name}
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    <div className="text-sm text-gray-500">
+                                                        {month.income}
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </ProtectedPage>
     );
 };
 
