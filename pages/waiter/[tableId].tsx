a5c8616a999cb7e4e882487c56fe9bea3ad4684e
Daniel Garnica <thedaniel115@gmail.com>
2022-01-14 23:49:13 -0600

    Updat Styles on TableDetail

--- pages/waiter/[tableId].tsx
+++ pages/waiter/[tableId].tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import { FaUtensils } from 'react-icons/fa';
 import { HiBan, HiClipboardCheck, HiCurrencyDollar, HiPlus } from 'react-icons/hi';
 import { Action, Fab } from 'react-tiny-fab';
+import BackButton from '../../components/buttons/BackButton';
 import Navbar from '../../components/layout/Navbar';
 import ProtectedPage from '../../components/ProtectedPage';
 import { useGetTableByIdQuery } from '../../graphql/graphql';
@@ -19,6 +20,7 @@ const TableDetail = () => {
         <ProtectedPage username="Mesero" redirectTo="/">
             <div className="min-h-screen p-8 bg-gray-200">
                 <Navbar />
+                <BackButton text="Regresar" pathNameOnBack="/waiter" />
                 <h1 className="text-3xl font-semibold text-brown">{data?.tableById?.name}</h1>
                 <Fab
                     icon={<HiPlus className="text-3xl" />}
@@ -38,9 +40,16 @@ const TableDetail = () => {
                     </Action>
 
                     <Action text="Cancelar Orden" style={{ background: '#b91c1c' }}>
-                        <HiBan className="text-xl bg-red" />
+                        <HiBan className="text-xl" />
                     </Action>
                 </Fab>
+
+                <div className="flex justify-center w-full mt-8">
+                    <div className="flex flex-col items-center gap-4 text-sm text-gray-500">
+                        <span>Aún no hay ningún platillo</span>
+                        <FaUtensils className="text-xl" />
+                    </div>
+                </div>
             </div>
         </ProtectedPage>
     );
